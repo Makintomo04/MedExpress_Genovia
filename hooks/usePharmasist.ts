63c0966b11f68fed6c4aad017f21150b277b25cc
Michael Akintomo <1721809@alumni.brunel.ac.uk>
2024-02-17 05:58:36 +0000

    changed randomuser endpoint

--- hooks/usePharmasist.ts
+++ hooks/usePharmasist.ts
@@ -5,7 +5,7 @@ const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export default function usePharmasist() {
   const { data, error, isLoading } = useSWR(
-    "https://randomuser.me/api/",
+    "https://randomuser.me/api/?nat=gb",
     fetcher,
     {}
   );
